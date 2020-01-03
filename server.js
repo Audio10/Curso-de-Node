e69f9bbdfb3c716b3cb0b27d8addaeaf87e78633
Claudio Vazquez <claudiojesus_10@hotmail.com>
2020-01-03 13:30:17 -0600

    5. Archivos estaticos y errores

--- server.js
+++ server.js
@@ -13,20 +13,22 @@ app.use(router)
 router.get('/message', function (req, res) {
     console.log(req.headers)
     res.header({
-        "custom-header": "Nuestro valor personalizado",
+        "custom-header": 'Nuestro valor personalizado',
     })
-    response.success(req,res, "Lista de mensajes")
+    response.success(req,res, 'Lista de mensajes')
 })
 
 router.post('/message', function (req, res) {
     console.log(req.query)
     console.log(req.body)
     if ( req.query.error === 'ok'){
-        response.error(req, res, 'Error simulado', 401)
+        response.error(req, res, 'Error inesperado', 500, 'Es solo una simulacion de los errores.')
     }else {
-        response.success(req, res, "Creado correctamente", 201)
+        response.success(req, res, 'Creado correctamente', 201)
     }
 })
+
+app.use('/app', express.static('public'))
 
 app.listen(3000)
 console.log('La aplicacion esta escuchando en http://localhost:3000')
